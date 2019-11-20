bdf32ec703b479168d59b23e3f015b9156371120
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-20 15:58:21 +0900

    MacOSのインストールスクリプトを作成した

---
+++ macos/brew.sh
@@ -0,0 +1,11 @@
+#!bin/bahs
+
+brew install fish
+brew install git
+brew install node
+brew install rbenv
+brew install ruby
+brew install tmux
+brew install vim
+brew install wget
+brew install zsh
