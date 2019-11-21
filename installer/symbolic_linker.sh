39118e8beda34f36a75eaa1ff0f062f465151a91
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-21 20:52:49 +0900

    vscodeのsettings.jsonを修正した

--- installer/symbolic_linker.sh
+++ installer/symbolic_linker.sh
@@ -2,11 +2,11 @@
 
 rm ~/.tmux.conf
 rm ~/.vimrc
-rm ~/Library/Application\ Support/Code/User/setting.json
+rm ~/Library/Application\ Support/Code/User/settings.json
 
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dotfiles/vscode/setting.json ~/Library/Application\ Support/Code/User/setting.json
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # install extention
 cat ~/dotfiles/vscode/extensions | while read line
