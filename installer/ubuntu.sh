578726f4a96194c25c23a15016c16e97ec17d271
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-12-25 22:44:24 +0900

    ubuntuのインストーラを追加した

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -1,3 +1,4 @@
 #!bin/bash
 
+sh ~/dotfiles/installer/ubuntu/apt.sh
 sh ~/dotfiles/installer/ubuntu/symbolic_linker.sh
---
+++ installer/ubuntu/apt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+## install
+sudo apt-get -y install tmux
+sudo apt-get -y install zsh
+sudo apt-get -y install fish
+sudo apt-get -y install htop
+sudo apt-get -y install iftop
+sudo apt-get -y install python
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install ruby
+sudo apt-get -y install golang
+sudo apt-get -y install gem
+sudo apt-get -y install make
+sudo apt-get -y install curl
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install nginx
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
