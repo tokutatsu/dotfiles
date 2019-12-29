b6bcc23653bb5791f6306f5cd315b3623bdd9001
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-12-30 02:37:14 +0900

    ubuntuのシンボリックリンクの設定を追加した

--- installer/ubuntu/symbolic_linker.sh
+++ installer/ubuntu/symbolic_linker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+rm ~/.bash_profile
+rm ~/.bashrc
+rm ~/.config/fish/config.fish
+rm -rf ~/.config/fish/functions
 rm ~/.tmux.conf
 rm ~/.vimrc
 
+ln -s ~/dotfiles/bash/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/fish/functions ~/.config/fish/functions
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
